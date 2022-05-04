4542dfdf03a4c5c310061ce09392eb89c7e26df9
Gold240sx <92943303+Gold240sx@users.noreply.github.com>
2022-05-04 14:49:18 -0500

    init commit

--- components/banner.js
+++ components/banner.js
@@ -3,7 +3,7 @@ import styles from './banner.module.css';
 const Banner = (props) => {
     return (
         <div className={styles.container}>
-            <h1>Coffee <span className={styles.titlewhite}>Connoisseur</span></h1>
+            <h1 className={styles.title}>Coffee <span>Connoisseur</span></h1>
             <p className={styles.subtitle}>Discover your local coffe Shops!</p>
             <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
         </div>
